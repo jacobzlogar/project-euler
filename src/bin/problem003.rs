5ed93b4518753464096a400f63fd4532df599ac2
Jacob zlogar <jacob@vaultinnovation.com>
2022-08-05 15:49:32 -0400

    problem #3

--- src/bin/problem003.rs
+++ src/bin/problem003.rs
@@ -1,35 +1,62 @@
-use std::fmt::Debug;
+// fn is_prime(value: usize) -> bool {
+//     if value == 2 || value == 3 {
+//         return true
+//     }
 
-#[derive(Debug,Copy,Clone)]
-struct Prime {
-    is_prime: bool,
-    number: u64,
-}
+//     if value % 2 == 0 || value % 3 == 0 {
+//         return false
+//     }
 
-fn largest_prime_factor(num: u64) -> u64 {
-    let mut primes: Vec<Prime> = vec![];
+//     let mut i: usize = 5;
+//     while i * i <= value {
+//         if value % i == 0 || value % (i + 2) == 0 {
+//             return false
+//         }
+//         i += 6;
+//     }
 
-    for i in (2..num).rev() {
-        let p = Prime {
-            is_prime: bool::from(true),
-            number: u64::from(i)
-        };
-        primes.push(p);
-    }
-
-    for x in &mut *primes {
-        // determine_prime(&mut p);
-        x.is_prime = bool::from(false);
-    }
-
-    println!("{:?}", primes);
-
-    num
-}
-
+//     return true
+// }
 fn main() {
-    // let prime = largest_prime_factor(600851475143);
-    let prime = largest_prime_factor(120);
+    let result = factors(600851475143);
+    println!("{:?}", &result);
+    println!("{}", result[result.len() - 1]);
+}
 
-    println!("{}", prime);
+fn is_even(value: usize) -> bool {
+    value % 2 == 0
+}
+
+struct Divisor {
+    value: usize
+}
+
+impl Divisor {
+    fn next_factor(&self, divisible: usize) -> usize {
+        let mut val: usize = self.value;
+        while divisible % val != 0 {
+            if is_even(val) {
+                val += 1
+            } else {
+                val += 2
+            }
+        }
+        val
+    }
+}
+
+fn factors(mut max: usize) -> Vec<usize> {
+    // start with a divisor at the smallest possible prime factor
+    let initial = Divisor { value: 2 };
+    // find the next divisor
+    let mut divisor = Divisor { value: initial.next_factor(max) };
+    let mut factors = vec![divisor.value];
+    // loop until the remainder is a prime number
+    while max / divisor.value != 1 {
+        max = max / divisor.value;
+        divisor = Divisor { value: divisor.next_factor(max) };
+        factors.push(divisor.value);
+    }
+
+    factors
 }
